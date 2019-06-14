39333acb8766284177f54b8f494cad26255b4507
Buuch1196 <jonas.guleruzh.ch>
2019-06-14 16:38:19 +0200

    test with array handling and for loops

--- BankingApp/main.swift
+++ BankingApp/main.swift
@@ -8,5 +8,20 @@
 
 import Foundation
 
-print("Hello, World!")
+var list=[Int]()
+
+for i in 1...5 {
+	list.append(i)
+	
+}
+
+for i in 0...list.count-1{
+	print(list[i])
+}
+
+/* Arrays starten bei 0
+count gibt die Länge zurück (wie Python)
+*/
+
+print("done")
 
