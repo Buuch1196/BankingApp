2e6fb507f8c9638f0ced477e6ea6a31425f87673
Buuch1196 <jonas.guleruzh.ch>
2019-06-14 16:10:06 +0200

    Initial Commit

---
+++ BankingApp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BankingApp
+//
+//  Created by Jonas Guler on 14.06.19.
+//  Copyright © 2019 Jonas Guler. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
